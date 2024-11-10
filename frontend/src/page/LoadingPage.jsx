cd83ca872d9231e8a7ed11fd8088cf2df87273ab
Chrisliang33 <liangxianmai@yahoo.com>
2024-11-10 11:12:59 -0500

    new

--- frontend/src/page/LoadingPage.jsx
+++ frontend/src/page/LoadingPage.jsx
@@ -4,10 +4,16 @@ import Footer from "../components/Footer";
 
 const LoadingPage = () => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <div>Loading...</div>
-
+      <div className="flex-grow flex items-center justify-center">
+        <div className="text-center">
+          <div className="text-4xl font-semibold text-blue-600 mb-4">
+            Loading...
+          </div>
+          <div className="animate-spin border-t-4 border-blue-600 w-16 h-16 rounded-full mx-auto"></div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
