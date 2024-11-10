b87cb96d338fa62c82ace3778f4596e0b6206cd0
Chrisliang33 <liangxianmai@yahoo.com>
2024-11-10 11:14:53 -0500

    new

--- frontend/src/page/RecapPage.jsx
+++ frontend/src/page/RecapPage.jsx
@@ -12,7 +12,7 @@ const RecapPage = () => {
   const web = "https://sports-ai.onrender.com";
   useEffect(() => {
     axios
-      .get(`${local}/yesterday_predictions`)
+      .get(`${web}/yesterday_predictions`)
       .then((response) => {
         setPredictions(response.data);
         setLoading(false);
